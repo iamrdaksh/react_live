0fc6b109921fac65254d6bce5ffed28b0fc54123
Rahul <iamrdaksh.abes@gmail.com>
2020-09-03 22:18:20 +0530

    todoList

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../mysass.scss';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 class TodoList extends Component {
     constructor(props) {
@@ -18,11 +19,21 @@ class TodoList extends Component {
         })
     }
     clickHandler = () => {
-        this.setState(prevState => ({
-            newList: [...prevState.newList, this.state.item]
-        }))
+        if (this.state.item) {
+            this.setState(prevState => ({
+                newList: [...prevState.newList, this.state.item]
+            }))
+        }
         this.setState({
             item: ''
+        })
+    }
+
+    deleteHandler = (index) => {
+        this.setState({
+            newList: this.state.newList.filter((arrElem,i) => {
+                return i !== index
+            })
         })
     }
 
@@ -30,9 +41,9 @@ class TodoList extends Component {
         return (
             <div className="todoContainer">
                 <h1>ToDo List</h1>
-                <div class="inputElement">
+                <div className="inputElement">
                     <div>
-                        <input type="text" placeholder="Add a Items" onChange={this.changeHandler} value={this.state.item}/>
+                        <input type="text" placeholder="Add a Items" onChange={this.changeHandler} value={this.state.item} />
                     </div>
                     <div className="plusIcon" onClick={this.clickHandler}>
                         <AddCircleOutlineIcon style={{ fontSize: 40 }} />
@@ -40,9 +51,13 @@ class TodoList extends Component {
                 </div>
                 <ol>
                     {
-                        this.state.newList.map((itemValue,index) => {
+                        this.state.newList.map((itemValue, index) => {
                             return (
-                                <li key={index}>{itemValue}</li>
+                                <li key={index} className="alignment">{itemValue}
+                                    <div style={{ display: 'inline-block', marginLeft: '20px' }}>
+                                        <DeleteIcon onClick={() => { this.deleteHandler(index) }} />
+                                    </div>
+                                </li>
                             )
                         })
                     }
